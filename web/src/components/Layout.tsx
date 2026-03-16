79793a9bf870f9560cb431c5cab31b028d24b76f
Brand201 <48253423+Brand201@users.noreply.github.com>
2026-03-16 18:15:56 +0000

    feat: WEB-FRONTEND-006 - Integrate a Headless UI Component Library

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { Button } from './Button';
 
 const Layout: React.FC = () => {
   return (
@@ -7,10 +8,14 @@ const Layout: React.FC = () => {
       <nav className="w-[200px] bg-gray-100 p-5 border-r border-gray-300">
         <ul className="list-none p-0">
           <li className="mb-2.5">
-            <Link to="/">Dashboard</Link>
+            <Button asChild className="w-full justify-start">
+              <Link to="/">Dashboard</Link>
+            </Button>
           </li>
           <li className="mb-2.5">
-            <Link to="/logs">Logs</Link>
+            <Button asChild className="w-full justify-start">
+              <Link to="/logs">Logs</Link>
+            </Button>
           </li>
         </ul>
       </nav>
